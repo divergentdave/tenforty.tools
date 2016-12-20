f812eee19afd96201d05af260db6ddbb91dab5d4
David Cook <divergentdave@gmail.com>
2016-12-19 20:42:58 -0600

    Write graphviz file showing line dependencies

--- src/tenforty/tools.clj
+++ src/tenforty/tools.clj
@@ -1,8 +1,34 @@
 (ns tenforty.tools
+  (use clojure.java.io)
   (use tenforty.core)
+  (require tenforty.forms.ty2015)
   (:gen-class))
 
+(defn dump-graphviz
+  []
+  (let [lines (apply concat (map #(vals %) (vals tenforty.forms.ty2015/forms)))]
+    (str "digraph tenforty {\n"
+         (apply str (map #(str "    \""
+                               (get-keyword %)
+                               "\" [label=\""
+                               (name (get-keyword %))
+                               "\"];\n")
+                         lines))
+         (apply str (map
+                      (fn [line] (apply str (map
+                                             (fn [dep] (str "    \""
+                                                            dep
+                                                            "\" -> \""
+                                                            (get-keyword line)
+                                                            "\";\n"))
+                                             (get-deps line))))
+                      lines))
+         "}\n"
+         )
+    ))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (with-open [wrtr (writer "graph.gv")]
+    (.write wrtr (dump-graphviz)))
+  )
