eda312c3746958b1c3b042ca913d7de0f61002b9
David Cook <divergentdave@gmail.com>
2016-12-25 10:16:51 -0600

    Handle groups in forms

--- test/tenforty/tools_test.clj
+++ test/tenforty/tools_test.clj
@@ -6,6 +6,5 @@
 (deftest graphviz-test
   (testing "Graphviz export"
     (is (= "digraph tenforty {\n    \":a\" [label=\"a\"];\n    \":b\" [label=\"b\"];\n    \":b\" -> \":a\";\n}\n"
-           (dump-graphviz {:form {:a (makeline :a (cell-value :b))
-                                  :b (->InputLine :b)}})))))
-
+           (dump-graphviz {:form {:group {:a (makeline :a (cell-value :b))
+                                          :b (->InputLine :b)}}})))))
