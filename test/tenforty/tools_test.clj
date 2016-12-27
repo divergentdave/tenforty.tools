a532e97241ad32de3b20a670526029f9a23098da
David Cook <divergentdave@gmail.com>
2016-12-26 21:44:49 -0600

    Update to use FormSubgraph

--- test/tenforty/tools_test.clj
+++ test/tenforty/tools_test.clj
@@ -6,5 +6,8 @@
 (deftest graphviz-test
   (testing "Graphviz export"
     (is (= "digraph tenforty {\n    \":a\" [label=\"a\"];\n    \":b\" [label=\"b\"];\n    \":b\" -> \":a\";\n}\n"
-           (dump-graphviz {:form {:a (make-formula-line :a (cell-value :b))
-                                  :b (make-input-line :b)}})))))
+           (dump-graphviz
+            (->FormSubgraph
+             {:a (make-formula-line :a (cell-value :b))
+              :b (make-input-line :b)}
+             {}))))))
